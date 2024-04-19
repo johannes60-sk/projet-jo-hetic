9b1724a3f117ae15d9ef05a4f3548230bb64fae0
Arouna Konake <arouna.akonake@gmail.com>
2024-04-19 16:00:02 +0200

    push

--- front/src/pages/AdminDashboard.jsx
+++ front/src/pages/AdminDashboard.jsx
@@ -1,10 +1,58 @@
 import React from "react";
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const AdminDashboard = () => {
   return (
-    <>
-      <div>Admin Dashboard </div>
-    </>
+    <div className="flex flex-col items-center">
+      <h1 className="font-bold text-3xl mt-4 mb-6">Admin Dashboard</h1>
+      <div className="flex justify-center w-3/5">
+        <div className="w-1/2">
+          <Command>
+            <CommandList>
+              <CommandGroup heading="Settings">
+                <CommandItem>Profile</CommandItem>
+                <CommandItem>Article</CommandItem>
+                <CommandItem>Settings</CommandItem>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </div>
+        <div className="w-1/2">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">ID</TableHead>
+                <TableHead>Nom</TableHead>
+                <TableHead>Prenom</TableHead>
+                <TableHead className="text-right">Adresse Mail</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
   );
 };
 
