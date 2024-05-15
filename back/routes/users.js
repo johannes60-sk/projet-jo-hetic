f1f116da87204bfa00111aa720b176d1e6317b79
hugolhld <hugo.lhernould@gmail.com>
2024-05-15 11:57:06 +0200

    feat: add users route

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,18 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+function generateToken(user) {
+    return jwt.sign({ email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '60s' });
+}
+
+router.post('/register', (req, res) => {
+    if (!req.body.username || !req.body.password || !req.body.email) {
+        return res.json({ result: false, error: 'Invalid input' });
+    } else {
+        User.findOne({ email: req.body.email }).then((data) => {
+            if (data === null) {
+                const newUser = new User({
+                    username: req.body.username,
+                    password: bcrypt.hashSync(req.body.password, 10),
+                    email: req.body.email,
+                    jwt: generateToken({ email: req.body.email }),
+                });
+    
+                newUser.save().then((newDoc) => {
+                    res.json({ result: true, user: newDoc });
+                });
+            } else {
+                res.json({ result: false, error: 'Email already registered' });
+            }
+        })
+    }
 });
 
-router.post('/register', async function(req, res, next) {
-  const { name, email, password, role } = req.body;
-  const user = new User({ name, email, password, role });
-  await user.save();
-  res.json({ message: 'User created' });
+router.post('/login', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.json({ result: false, error: 'Invalid input' });
+    } else {
+        User.findOne({ email: req.body.email }).then((data) => {
+            if (data === null) {
+                res.json({ result: false, error: 'Invalid credentials' });
+            } else {
+                const isPasswordValid = bcrypt.compareSync(req.body.password, data.password);
+                if (isPasswordValid) {
+                    // res.json({ result: true, user: data });
+                    User.findOneAndUpdate({ email: req.body.email }, { jwt: generateToken({ email: req.body.email }) })
+                        .then((data) => { res.json({ result: true, user: data }) });
+                } else {
+                    res.json({ result: false, error: 'Wrong password' });
+                }
+                // isPasswordValid ? res.json({ result: true, user: data }) : res.json({ result: false, error: 'Wrong password' });
+            }
+        })
+    }
 });
 
 module.exports = router;
