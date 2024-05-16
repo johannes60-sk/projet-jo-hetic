6297b522ea5a6cefbbcd1471bc05f3183c846937
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2024-05-16 15:45:54 +0200

    add fake data news

--- front/src/pages/News.jsx
+++ front/src/pages/News.jsx
@@ -1,29 +1,37 @@
 import { Input } from "@/components/ui/input"
+import { useEffect, useState } from "react"
 
 const News = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5174/api/news')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(e => console.error(e))
+    }, [])
+
     return (
         <>
             <h1 className='text-2xl font-bold text-center my-4'>Liste des News</h1>
             <Input className='mx-auto my-4 w-3/5 rounded-md border-gray-600 placeholder:text-opacity-80' />
-            <div class="divide-y divide-dashed">
-                <div className="mx-4 content-center mb-5">
-                    <a href = "#">
-                    <div>
-                        <img className='size-1/4 float-left mr-3' alt="Site" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQU8ylKN8VWso8y8rr1so6bulaJerZr-YMcsYoBITfsg&s" />
-                        <h1 className='text-2xl font-bold my-4 scroll-ml-96'><a href = "#">Site</a></h1>
-                        <p className="scroll-ml-16">Ipnum</p>
+            <div className="divide-y divide-dashed">
+            {
+                data.length > 0 ? data.map((news, index) => (
+                    <div className="mx-4 content-center mb-5" id={index}>
+                        <a href = "#">
+                        <div>
+                            <img className='size-1/4 float-left mr-3' alt={news.title} src={news.image} />
+                            <h1 className='text-2xl font-bold my-4 scroll-ml-96'>{news.title}</h1>
+                            <p className="scroll-ml-16">{news.description}</p>
+                        </div>
+                        </a>
                     </div>
-                    </a>
-                </div>
-                <div className="mx-4 content-center mb-5">
-                    <a href = "#">
-                    <div>
-                        <img className='size-1/4 float-left mr-3' alt="Site" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQU8ylKN8VWso8y8rr1so6bulaJerZr-YMcsYoBITfsg&s" />
-                        <h1 className='text-2xl font-bold my-4 scroll-ml-96'><a href = "#">Site</a></h1>
-                        <p className="scroll-ml-16">Ipnum</p>
-                    </div>
-                    </a>
-                </div>
+                ))
+                :
+                <p className="text-center">Aucune donnée disponible</p>
+            }
             </div>
         </>
     );
